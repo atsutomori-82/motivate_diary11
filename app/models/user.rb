12db983e2876a9397e7b62c774a25e8e411bf7dc
atsutomori-82 <atsutomori.82@gmail.com>
2020-12-26 09:10:12 +0900

    validationの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :diary
+
+  with_option presence: true do
+    validates :nickname, length: { maximum: 10 }
+  end
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  
 end
