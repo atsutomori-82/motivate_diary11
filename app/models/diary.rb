76a858bcb80beb52fdc41f16da23c3bebd0e1c36
atsutomori-82 <atsutomori.82@gmail.com>
2020-12-29 12:55:51 +0900

    バリデーションの作成

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -2,5 +2,14 @@ class Diary < ApplicationRecord
   belongs_to :users
   has_many :motivate
 
-  
+  with_options presence: true do
+    validates :calendar_date
+    validates :text
+  end
+
+  with_options  numericality: { other_than: 1 } do
+    validates :condition_id
+    validates :motivation_id
+    validates :work_volume_id
+  end
 end
